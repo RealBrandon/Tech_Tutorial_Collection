b436cfbf889fe3b09c45df5bd277687ec24aa307
RealBrandon <83159415+RealBrandon@users.noreply.github.com>
2022-04-14 23:25:38 +0800

    Auto installer for Edge Beta and VS Code
    This installer is for use on Fedora.

---
+++ vscode_edge_installer_fedora.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Import Microsoft GPG key
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+# Install VS Code
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+sudo dnf install code
+
+# Install Edge Beta
+sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
+sudo mv /etc/yum.repos.d/packages.microsoft.com_yumrepos_edge.repo /etc/yum.repos.d/microsoft-edge-beta.repo
+sudo dnf install microsoft-edge-beta
